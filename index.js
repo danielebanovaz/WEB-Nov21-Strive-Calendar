c718e68806c874e5dee7a0612182c12de45e5cfe
Daniele Banovaz <daniele.banovaz@gmail.com>
2021-12-09 09:56:20 +0100

    We are now generating days programmatically

---
+++ index.js
@@ -0,0 +1,26 @@
+
+const createDays = function() {
+
+    // Find the parent for the days (= month container)
+    let monthContainerNode = document.getElementById("month-container")
+
+    // TODO: Get the current month, so we can know how many days we need
+    // (just for now, we assume we are in December and we have 31 days)
+    let daysInTheMonth = 31
+
+    // Within a loop, we create as many days as we need
+    for (let dayNumber = 1; dayNumber <= daysInTheMonth; dayNumber++) {
+
+        // We create a new DIV element...
+        let newDayNode = document.createElement("div") // <div></div>
+        newDayNode.innerText = dayNumber // <div>1</div>
+        newDayNode.classList.add("day") // <div class="day">1</div>
+
+        // ...and we attach it as the last child of the month container
+        monthContainerNode.appendChild(newDayNode)
+    }
+}
+
+window.onload = function() {
+    createDays()
+}
