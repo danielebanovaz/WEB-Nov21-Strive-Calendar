30ad3f26211121ce32b1bd908758a647557d02f1
Daniele Banovaz <daniele.banovaz@gmail.com>
2021-12-09 10:43:37 +0100

    We can now select a single day by clicking on it

--- index.js
+++ index.js
@@ -20,7 +20,37 @@ const createDays = function() {
         newDayNode.innerText = dayNumber // <div>1</div>
         newDayNode.classList.add("day") // <div class="day">1</div>
 
+        // Set the 'selectDay' function as a LISTENER for the CLICK event
+        // on the newly created HTML node
+        newDayNode.onclick = selectDay
+        // (this is another, almost equivalent alternative)
+        // newDayNode.addEventListener("click", selectDay)
+
         // ...and we attach it as the last child of the month container
         monthContainerNode.appendChild(newDayNode)
     }
+}
+
+const getCurrentlySelectedDay = function() {
+    return document.querySelector(".selected")
+
+    //return document.getElementsByClassName("selected")[0]
+}
+
+const selectDay = function(e) {
+
+    // Let's find the currently selected day...
+    const currentlySelectedDay = getCurrentlySelectedDay()
+
+    // ...if there's one, DE-SELECT it
+    if (currentlySelectedDay !== null) {
+        // ...by removing the 'selected' class
+        currentlySelectedDay.classList.remove("selected")
+    }
+
+    // Find the element that has been click
+    const clickedDayNode = e.target
+
+    // Set its class to be 'selected'
+    clickedDayNode.classList.add("selected")
 }
